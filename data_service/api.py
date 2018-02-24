2a71df6f0f9282f7ffa6a21d06b1e97ea6157f2d
Derek Schaefer <schaefertek@gmail.com>
2018-02-24 00:12:48 -0500

    Changed listen port on data service (5000->5001)

--- data_service/api.py
+++ data_service/api.py
@@ -42,4 +42,4 @@ def health():
 
 if __name__ == "__main__":
     dao = MongoDao(os.environ['MONGODB']);
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5001, debug=False, threaded=True)
